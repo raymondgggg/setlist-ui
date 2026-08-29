74f0ff9f3a8c637e15e3d43a6ee4639747e942cf
Brent Nishioka <brentnishioka@gmail.com>
2026-08-29 12:27:31 -0700

    Prevent vite from clearing screen on container startup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tanstackRouter from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
+  clearScreen: false,
   plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react()],
   server: {
     host: true,
